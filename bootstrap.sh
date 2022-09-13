2731a05343cd62711b180f0b9e2031f3b5e976b1
zrt <1276096267@qq.com>
2022-09-13 17:47:10 +0800

    修改zsh插件存放的位置

---
+++ bootstrap.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env zsh
+
+DOTDIR=~/.dotfiles
+
+# source init.sh
+touch .zshrc
+sed -i "\:$DOTDIR/zsh/zshrc:d" ~/.zshrc
+echo "source $DOTDIR/zsh/zshrc" >> ~/.zshrc
+source ~/.zshrc
+
+# source vimrc.vim
+touch ~/.vimrc
+sed -i "\:$DOTDIR/vim/vimrc.vim:d" ~/.vimrc
+echo "source $DOTDIR/vim/vimrc.vim" >> ~/.vimrc
+
+# source tmux.conf
+touch ~/.tmux.conf
+sed -i "\:$DOTDIR/tmux/tmux.conf:d" ~/.tmux.conf
+echo "source-file $DOTDIR/tmux/tmux.conf" >> ~/.tmux.conf
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+  echo "Cloning tpm"
+  git clone -q --depth 1 https://kgithub.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
