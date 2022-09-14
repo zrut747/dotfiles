6cbf905011325bd2552223faf8ac2738032c8136
zrt <1276096267@qq.com>
2022-09-14 10:24:25 +0800

    移除starship，使用自定义prompt

---
+++ zsh/plugin.zsh
@@ -0,0 +1,37 @@
+# declare a simple plugin-load function
+function plugin-load() {
+  local repo plugin_name plugin_dir initfile initfiles
+  ZPLUGINDIR=~/.config/zsh/plugins
+  for repo in $@; do
+    plugin_name=${repo:t}
+    plugin_dir=$ZPLUGINDIR/$plugin_name
+    initfile=$plugin_dir/$plugin_name.plugin.zsh
+    if [[ ! -d $plugin_dir ]]; then
+      echo "Cloning $repo"
+      git clone -q --depth 1 --recursive --shallow-submodules https://kgithub.com/$repo $plugin_dir
+    fi
+    if [[ ! -e $initfile ]]; then
+      initfiles=($plugin_dir/*.plugin.{z,}sh(N) $plugin_dir/*.{z,}sh{-theme,}(N))
+      [[ ${#initfiles[@]} -gt 0 ]] || { echo >&2 "Plugin has no init file '$repo'." && continue }
+      ln -s "${initfiles[1]}" "$initfile"
+    fi
+    fpath+=$plugin_dir
+    (( $+functions[zsh-defer] )) && zsh-defer . $initfile || . $initfile
+  done
+}
+
+# make a github repo plugins list
+plugins=(
+  rupa/z
+  jeffreytse/zsh-vi-mode
+
+  # load these at hypersonic load speeds with zsh-defer
+  romkatv/zsh-defer
+  peterhurford/up.zsh
+  zsh-users/zsh-autosuggestions
+  zdharma-continuum/fast-syntax-highlighting
+)
+plugin-load $plugins
+
+# change the escape key to `jj` in insert mode
+ZVM_VI_INSERT_ESCAPE_BINDKEY=jj
---
+++ zsh/prompt.zsh
@@ -0,0 +1,21 @@
+# print prompt with colors
+autoload -U colors && colors
+
+function precmd() {
+    # Print a newline before the prompt, unless it's the
+    # first prompt in the process.
+    if [ -z "$NEW_LINE_BEFORE_PROMPT" ]; then
+        NEW_LINE_BEFORE_PROMPT=1
+    elif [ "$NEW_LINE_BEFORE_PROMPT" -eq 1 ]; then
+        echo
+    fi
+}
+
+local ret_status="%(?,%{$terminfo[bold]$fg[green]%}➜ ,%{$terminfo[bold]$fg[red]%}➜ "
+
+PROMPT="%(#,%{$fg[red]%}%n,%{$fg[cyan]%}%n) \
+%{$reset_color%}at %{$fg[green]%}%M \
+%{$fg[white]%}in \
+%{$terminfo[bold]$fg[green]%}%~%{$reset_color%}
+${ret_status}%{$reset_color%} \
+"
