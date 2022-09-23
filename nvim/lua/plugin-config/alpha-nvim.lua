49d027aa046687f01e10df19f61df8e7fb073250
zrt <1276096267@qq.com>
2022-09-23 14:24:25 +0800

    修改alpha.nvim配置

--- nvim/lua/plugin-config/alpha-nvim.lua
+++ nvim/lua/plugin-config/alpha-nvim.lua
@@ -1,3 +1,14 @@
-require("alpha").setup(
-  require "alpha.themes.dashboard".config
-)
+local alpha = require("alpha")
+local dashboard = require("alpha.themes.dashboard")
+
+-- Set menu
+dashboard.section.buttons.val = {
+    dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
+    dashboard.button( "f", "  > Find file", ":cd $HOME/workspace | Telescope find_files<CR>"),
+    dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
+    dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+    dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
+}
+
+-- Send config to alpha
+alpha.setup(dashboard.opts)
