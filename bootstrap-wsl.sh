42b0c48b0e0ad36b65832d06b989a73299b15b0d
zrt <1276096267@qq.com>
2022-09-23 09:56:25 +0800

    添加wsl配置

---
+++ bootstrap-wsl.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+DOTDIR=~/.dotfiles
+
+# wsl 启动配置
+if [[ $(uname -r) =~ WSL2 ]]; then
+  if [ ! -d /etc/wsl ];then
+    sudo mkdir /etc/wsl
+  fi
+  sudo cp -r $DOTDIR/wsl/wsl.conf /etc
+  sudo cp -r $DOTDIR/wsl/init.sh /etc/wsl
+fi
--- wsl/init.sh
+++ wsl/init.sh
@@ -1,5 +1,4 @@
 # 在 hosts 中添加 win.local 表示宿主机，方便 wsl 访问宿主机网络
-# 宿主机需要修改防火墙配置（New-NetFirewallRule -DisplayName "WSL" -Direction Inbound  -InterfaceAlias "vEthernet (WSL)"  -Action Allow)
 WSL_HOST_IP=$(cat /etc/resolv.conf |grep -oP '(?<=nameserver\ ).*')
 sed -i "\:win\.local:d" /etc/hosts
 echo "$WSL_HOST_IP win.local" >> /etc/hosts
