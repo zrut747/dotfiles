e8edac680610d25919ae4cfbe36ec121f4c1a2a7
zrt <1276096267@qq.com>
2022-10-15 22:53:19 +0800

    修复cmp_nvim_lsp配置使用废弃api导致的错误

--- nvim/lua/lsp/config/default.lua
+++ nvim/lua/lsp/config/default.lua
@@ -1,6 +1,7 @@
 -- local navic = require("nvim-navic")
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local opt = {
-  capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+  capabilities = capabilities,
   flags = {
     debounce_text_changes = 150,
   },
