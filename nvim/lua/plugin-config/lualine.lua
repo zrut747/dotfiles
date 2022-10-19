fe826afe781c64c9657f5f2aef6122cb03323e4a
zrt <1276096267@qq.com>
2022-10-19 10:58:19 +0800

    添加tokynight主题，修改默认主题为tokynight

--- nvim/lua/plugin-config/lualine.lua
+++ nvim/lua/plugin-config/lualine.lua
@@ -8,7 +8,8 @@ lualine.setup({
   options = {
     -- 指定皮肤
     -- https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
-    theme = "gruvbox",
+    -- theme = "gruvbox",
+    theme = "tokyonight",
     -- 分割线
     component_separators = {
       left = "|",
