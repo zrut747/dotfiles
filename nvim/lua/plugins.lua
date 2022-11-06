31465b6feda17fb4bfa15c129ffc8502c0760488
zrt <1276096267@qq.com>
2022-11-06 22:42:23 +0800

    修改Packer输出为浮窗形式

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -33,6 +33,15 @@ if not status_ok then
   return
 end
 
+-- Have packer use a popup window
+packer.init({
+  display = {
+    open_fn = function()
+      return require("packer.util").float({ border = "rounded" })
+    end,
+  },
+})
+
 packer.startup({
   function(use)
     -- Packer can manage itself
