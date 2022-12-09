5efa9027da4ae04c5b2a9a6a4c61f16e10837876
zrt <1276096267@qq.com>
2022-12-09 09:33:44 +0800

    treesitter添加新语言

--- nvim/lua/plugin-config/nvim-treesitter.lua
+++ nvim/lua/plugin-config/nvim-treesitter.lua
@@ -7,7 +7,7 @@ end
 require("nvim-treesitter.configs").setup {
   -- 安装 language parser
   -- :TSInstallInfo 命令查看支持的语言
-  ensure_installed = { "html", "css", "vim", "lua", "javascript", "typescript", "tsx", "vue" },
+  ensure_installed = { "html", "css", "vim", "lua", "javascript", "typescript", "tsx", "vue", "rust", "java", "python" },
   -- 启用代码高亮功能
   highlight = {
     enable = true,
