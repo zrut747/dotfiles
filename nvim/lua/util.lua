4382f01a42befa767e960a85de0a76ef79473e51
zrt <1276096267@qq.com>
2023-02-03 15:59:03 +0800

    nvim utils增加方法

--- nvim/lua/util.lua
+++ nvim/lua/util.lua
@@ -13,4 +13,14 @@ function M.github_mirror()
   end
 end
 
+function M.on_attach(on_attach)
+  vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+      local buffer = args.buf
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      on_attach(client, buffer)
+    end,
+  })
+end
+
 return M
