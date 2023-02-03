68be5b7eadbcc6bd0c81dba13d1cd3504f73c18e
zrt <1276096267@qq.com>
2023-02-03 15:18:47 +0800

    增加彩虹括号插件

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,8 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "p00f/nvim-ts-rainbow",
+  },
   build = ":TSUpdate",
   opts = {
     -- 安装 language parser
@@ -21,7 +24,16 @@ return {
     -- 启用代码高亮功能
     highlight = {
       enable = true,
-      additional_vim_regex_highlighting = false
+      additional_vim_regex_highlighting = false,
+    },
+    -- 彩虹括号
+    rainbow = {
+      enable = true,
+      -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+      extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+      max_file_lines = nil, -- Do not enable for files with more than n lines, int
+      -- colors = {}, -- table of hex strings
+      -- termcolors = {} -- table of colour name strings
     },
     -- 启用增量选择
     incremental_selection = {
