2a975060f8e9087a1291858cd4049e238cc8afec
zrt <rt.zhuang@outlook.com>
2024-06-05 21:57:05 +0800

    添加awesome lsp配置

--- nvim/lua/plugins/lsp/config/lua_ls.lua
+++ nvim/lua/plugins/lsp/config/lua_ls.lua
@@ -8,21 +8,30 @@ M.opts = {
   settings = {
     Lua = {
       runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
         version = "LuaJIT",
-        -- Setup your lua path
         path = runtime_path,
       },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = { "vim" },
-      },
       workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-        checkThirdParty = false,
+        library = {
+          "/usr/share/nvim/runtime/lua",
+          "/usr/share/nvim/runtime/lua/lsp",
+          "/usr/share/awesome/lib",
+        },
       },
-      -- Do not send telemetry data containing a randomized but unique identifier
+      completion = {
+        enable = true,
+      },
+      diagnostics = {
+        enable = true,
+        globals = {
+    --     -- vim全局配置
+          "vim",
+          -- awesome全局配置
+          "awesome",
+          "client",
+          "root",
+        },
+      },
       telemetry = {
         enable = false,
       },
