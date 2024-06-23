56f08d7d7e2cf364ca47d9d2286d43ee82ed6986
zrt <rt.zhuang@outlook.com>
2024-06-23 14:48:16 +0800

    添加greenclip

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -11,3 +11,4 @@ run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
 run feh --randomize --bg-fill ~/.config/bspwm/wallpapers/ &
 run ~/.config/bspwm/polybar/launch.sh &
 run picom --config ~/.config/bspwm/picom/picom.conf &
+run greenclip daemon>/dev/null &
